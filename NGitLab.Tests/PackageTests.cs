b81d6b70f1f91784f55b82353ad94ec9319c6372
Andreas Grünwald <andreas@grynwald.net>
2024-05-05 17:22:24 +0200

    Avoid usage of file system in PackageTests

--- NGitLab.Tests/PackageTests.cs
+++ NGitLab.Tests/PackageTests.cs
@@ -1,5 +1,6 @@
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using NGitLab.Models;
 using NGitLab.Tests.Docker;
@@ -17,13 +18,15 @@ namespace NGitLab.Tests
             var project = context.CreateProject();
             var packagesClient = context.Client.Packages;
 
+            using var contentStream = CreateMemoryStream("Some Package Content");
+
             var packagePublish = new PackagePublish
             {
                 FileName = "README.md",
                 PackageName = "Packages",
                 PackageVersion = "1.0.0",
                 Status = "default",
-                PackageStream = File.OpenRead("../../../../README.md"),
+                PackageStream = contentStream,
             };
 
             var newGenericPackage = await packagesClient.PublishGenericPackageAsync(project.Id, packagePublish);
@@ -35,6 +38,12 @@ namespace NGitLab.Tests
             Assert.AreEqual(1, genericPackages.Count);
             Assert.AreEqual(newGenericPackage.PackageId, genericPackages[0].PackageId);
             Assert.AreEqual(singleGenericPackage.PackageId, newGenericPackage.PackageId);
+        }
+
+        private static MemoryStream CreateMemoryStream(string content)
+        {
+            var bytes = Encoding.UTF8.GetBytes(content);
+            return new MemoryStream(bytes);
         }
     }
 }
